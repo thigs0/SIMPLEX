f910a966d90171c523f202313b9a1336aa71f81c
Thigs <tthiagosantos38@gmail.com>
2023-09-19 23:01:06 -0300

    second

--- sistemas.jl
+++ sistemas.jl
@@ -1,7 +1,17 @@
 module sistemas
 
     function solveQR(Q, R, b)
-        return "o"
+        y = Q'*b
+        n = sizeof(R)
+        resul = copy(y)
+
+        for i in (n-1):-1:1
+            for j in (i+1):n
+                resul-=R[i,j]                
+            end
+            resul /= R[i,i]
+        end
+        return resul
     end
 
     function solveQRt(Q, R, b)
