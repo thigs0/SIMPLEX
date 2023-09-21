84e839f207dce0a34824086f703cdbac974ad9df
Thigs <tthiagosantos38@gmail.com>
2023-09-20 23:12:52 -0300

    att

--- Simplex.jl
+++ Simplex.jl
@@ -2,30 +2,30 @@ include("./sistemas.jl")
 import .sistemas:solveQR, solveQRt
 using LinearAlgebra:qr
 
-function Simplex(A, b,c )
+function Simplex(A, b, c)
 
     #= A(matrix) associada ao problema Ax=b 
        b(Vetor) associado ao problema Ax=b
        c(Vetor) índices dos custos 
 
     =#
-    n,m = sizeof(A)
-    ccn = zeros(n-m)
+    n,m = size(A) # n linhas de A, m colunas de A
+    ccn = zeros(n-m) # cria um vetor nulo para armazenar custo não básico de tamanho n-m
 
     B,N, cb,cn = dual(a, b, c) # Passa as bases por referência
 
-    Q, R = qr(B)
-    xcb = solveQR(Q, R, b)
+    Q, R = qr(B) # Realiza a decomposição QR da matriz B
+    xcb = solveQR(Q, R, b) #encontra a solução básica factível
     for i in xcb # Se algum valor é negativo
         if i < 0
             error("O sistema não têm solução")
         end
     end
 
-    f = cb'*xcb;
+    f = cb'*xcb; # Calcula o valor da função
 
-    #custos relaticos
-    λ = solveQRt(Q,R,cb)
+#custos relaticos
+    λ = solveQRt(Q,R,cb) # Calcula o vetor multiplicador simplex
     
     for i in 1:(n-m)
         ccn = cn[i] - λ'N[:, i]
@@ -36,14 +36,14 @@ function Simplex(A, b,c )
     end
     pentra = argmin(ccn) # posição que iremos alterar para entrar na base
 
-    y = solveQR(Q, R, N[:,pentra])
-    εc = ones(m)*Inf
+    y = solveQR(Q, R, N[:,pentra]) # encontra o passo simplex
+    εc = ones(m)*Inf #Cria um vetor de tamanho m com valores infinitos
     for i in 1:m
         if y[i] > 0
             εc[i] = xcb/y[i]
         end
     end
-    psai = argmin(εc)
+    psai = argmin(εc) # encontra o mínimo, como o vetor tem componentes infinitas, não se preocupe com y negativo
 
 
     #Atualiza geral
--- sistemas.jl
+++ sistemas.jl
@@ -1,6 +1,13 @@
 module sistemas
 
     function solveQR(Q, R, b)
+        #=Q(Matrix nxn)
+          R(matrix nxn)
+          b(vetor n)
+
+          resolve o sistema Ax=b, com decomposição Q,R dada previamente
+
+        =#
         y = Q'*b
         n = size(R)[1]
         resul = copy(y)
@@ -14,10 +21,16 @@ module sistemas
     end
 
     function solveQRt(Q, R, b)
-        
-        n = size(R)[1]
-        resul = copy(b)
-        R=R'
+        #=Q(Matrix nxn)
+          R(matrix nxn)
+          b(vetor n)
+
+          resolve o sistema (A)tx=b, com decomposição Q,R dada previamente
+
+        =#
+        n = size(R)[1] #quantidade de linhas em R
+        resul = copy(b) #copia de b
+        R=R' #transposta da R
         for i in 1:n
             for j in 1:i-1
                 resul[i] -= R[i,j]*resul[j]                
