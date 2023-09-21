e3aa2da8d447a8ee9ddc519f21a3d9a16feb16a0
Thigs <tthiagosantos38@gmail.com>
2023-09-20 22:53:32 -0300

    QR

--- sistemas.jl
+++ sistemas.jl
@@ -2,20 +2,29 @@ module sistemas
 
     function solveQR(Q, R, b)
         y = Q'*b
-        n = sizeof(R)
+        n = size(R)[1]
         resul = copy(y)
-
-        for i in (n-1):-1:1
-            for j in (i+1):n
-                resul-=R[i,j]                
+        for i in n:-1:1
+            for j in i+1:n
+                resul[i] -= R[i,j]*resul[j]                
             end
-            resul /= R[i,i]
+            resul[i] /= R[i,i]
         end
         return resul
     end
 
     function solveQRt(Q, R, b)
-
+        
+        n = size(R)[1]
+        resul = copy(b)
+        R=R'
+        for i in 1:n
+            for j in 1:i-1
+                resul[i] -= R[i,j]*resul[j]                
+            end
+            resul[i] /= R[i,i]
+        end
+        return Q*resul
     end
 
 end
