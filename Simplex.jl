e7ebbef82f5ebab298393bf2a253646768343283
Thigs <tthiagosantos38@gmail.com>
2023-09-21 20:45:18 -0300

    funções

--- Simplex.jl
+++ Simplex.jl
@@ -1,6 +1,6 @@
 include("./funcoes_simplex.jl")
 
-function Simplex(A, b, c)
+function Simplex(A::Matrix, b::Vector, c::Vector)
 
     #= A(matrix) associada ao problema Ax=b 
        b(Vetor) associado ao problema Ax=b
@@ -10,40 +10,31 @@ function Simplex(A, b, c)
     n,m = size(A) # n linhas de A, m colunas de A
     ccn = zeros(n-m) # cria um vetor nulo para armazenar custo não básico de tamanho n-m
 
-    B,N, cb,cn = fase1(a, b, c) # Passa as bases por referência
+    B, N, xb, xn, cb, cn = fase1(a, b, c) # Passa as bases por referência
+    #xb e xn são os índices do x na base e não base
 
-    Q, R = qr(B) # Realiza a decomposição QR da matriz B
-    xcb = solveQR(Q, R, b) #encontra a solução básica factível
-    for i in xcb # Se algum valor é negativo
-        if i < 0
-            error("O sistema não têm solução")
+    while true # ele acha a solução ou quebra, o que acontecer primeiro
+        Q, R = qr(B) # Realiza a decomposição QR da matriz B
+        xcb = solveQR(Q, R, b) #encontra a solução básica factível
+        for i in xcb # Se algum valor é negativo
+            if i < 0
+                error("O sistema não têm solução")
+            end
         end
-    end
 
-    f = cb'*xcb; # Calcula o valor da função
+        f = cb'*xcb; # Calcula o valor da função
 
-#custos relaticos
-    λ = solveQRt(Q,R,cb) # Calcula o vetor multiplicador simplex
-    
-    for i in 1:(n-m)
-        ccn = cn[i] - λ'N[:, i]
-    end
-
-    if min(ccn) < 0 #Teste de otimalidade
-        return xb
-    end
-    pentra = argmin(ccn) # posição que iremos alterar para entrar na base
-
-    y = solveQR(Q, R, N[:,pentra]) # encontra o passo simplex
-    εc = ones(m)*Inf #Cria um vetor de tamanho m com valores infinitos
-    for i in 1:m
-        if y[i] > 0
-            εc[i] = xcb/y[i]
+        pentra = Custo_relativo(Q, R, cb)
+        if pentra == "ótimo" # se já estamos em um ótimo
+            return var_deci(xbc, xb)
         end
+        # Se não entrou, pentra mostra a posição que entra na base
+
+        psai = Direcao_simplex(Q, R, N, xcb, m) # retorna o índice que sai da base
+        #Atualiza geral
+
+        B, N, xb, xn, cb, cn = Atualiza(B, N, xb, xn, cb, cn, pentra, psai)
     end
-    psai = argmin(εc) # encontra o mínimo, como o vetor tem componentes infinitas, não se preocupe com y negativo
 
-
-    #Atualiza geral
 
 end
--- funcoes_simplex.jl
+++ funcoes_simplex.jl
@@ -39,3 +39,55 @@ function solveQRt(Q::Matrix, R::Matrix, b::Vector)
     end
     return Q*resul
 end
+
+function Custo_relativo(Q::Matrix, R::matrix, cb::Vector)
+    #=
+
+    Encontra a posição que sai da base, usando o custo relativo
+    =#
+    #custos relaticos
+    λ = solveQRt(Q,R,cb) # Calcula o vetor multiplicador simplex
+    
+    for i in 1:(n-m)
+        ccn = cn[i] - λ'N[:, i]
+    end
+
+    if min(ccn) < 0 #Teste de otimalidade
+        return "ótimo"
+    end
+    return = argmin(ccn) # posição que iremos alterar para entrar na base
+end
+
+function Direcao_simplex(Q, R, N, xcb, m)
+    #=
+
+    =#
+    y = solveQR(Q, R, N[:,pentra]) # encontra o passo simplex
+    εc = ones(m)*Inf #Cria um vetor de tamanho m com valores infinitos
+    for i in 1:m
+        if y[i] > 0
+            εc[i] = xcb/y[i]
+        end
+    end
+    return argmin(εc) # encontra o mínimo, como o vetor tem componentes infinitas, não se preocupe com y negativo
+end
+
+function var_deci(xbc::Vector, xb::Vector)
+    #=
+    Retorna as variáveis de decição
+    =#
+    n = size(xbc)
+    x = zeros(n)
+    for i in 1:N
+        if xb[i] < N
+            x[ xb[i] ] = xb[i]
+        end
+    end
+    return x
+end
+
+function Atualiza(B, N, xb, xn, cb, cn, pentra, psai)
+    #=
+        Com base no pentra e psai, atualiza a base e a não base
+    =#
+end
